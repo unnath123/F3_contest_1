f641e9ac940326c4e42f52df22ef7afc07299fba
unnath123 <unnathpgowda@gmail.com>
2023-09-17 01:06:46 +0530

    loader added

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
-const Api_key = "95751bfe"
-let searchele = document.getElementById("searchVal")
+const Api_key = "95751bfe";
+let searchele = document.getElementById("searchVal");
 
-
-function renderUi(array){
-    const mainContainer = document.getElementById("m-cont");
-    // console.log(array.Search[0])
-    for(let i=0;i<10;i++){
-        const divEle = document.createElement("div");
-        divEle.className = "item1";
-        divEle.innerHTML = `
+function renderUi(array) {
+  const mainContainer = document.getElementById("m-cont");
+  // console.log(array.Search[0])
+  for (let i = 0; i < 10; i++) {
+    const divEle = document.createElement("div");
+    divEle.className = "item1";
+    divEle.innerHTML = `
                 <div class="image1"><img src=${array.Search[i].Poster} alt="poster"></div>
                 <div class="number"><h1 class="num">${i}</h1></div>
-                <div class="title">${array.Search[i].Title}</div>`  
-    
-        mainContainer.appendChild(divEle) 
-    }
+                <div class="title">${array.Search[i].Title}</div>`;
+
+    mainContainer.appendChild(divEle);
+  }
 }
 
-async function fetchDetails(){
-let SearchVal = searchele.value;
-// let SearchVal = "Jurassic"
-let response = await fetch(`http://www.omdbapi.com/?s=${SearchVal}&apikey=95751bfe`);
-let result = await response.json();
-let brr = result.Search
-// console.log(brr)
 
-renderUi(result)
+function renderLoader(){
+    const m_container = document.getElementById("m-cont");
+    const progress = document.createElement("div");
+    progress.className = "progress";
+    progress.innerHTML = `
+    <div class="color"></div>`
+
+    m_container.appendChild(progress)
 }
 
-document.getElementById("submit").addEventListener("click",fetchDetails)
+function removeLoader(){
+    const progress = document.querySelector(".progress");
+    progress.remove();
+}
 
-// fetchDetails()
+
+async function fetchDetails() {
+  //let SearchVal = searchele.value;
+   let SearchVal = "Jurassic"
+
+    renderLoader()
+
+    let response = await fetch(
+    `http://www.omdbapi.com/?s=${SearchVal}&apikey=95751bfe`
+  );
+
+  let result = await response.json();
+
+  removeLoader()
+
+//   let brr = result.Search;
+  console.log(result);
+
+   renderUi(result)
+}
+
+ document.getElementById("submit").addEventListener("click", fetchDetails);
+
+//fetchDetails();
